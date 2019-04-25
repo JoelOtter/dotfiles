15f0239f32e559dee9e3cb82647a60bbfdd1b028
Joel Auterson <joel@improbable.io>
2019-04-25 15:45:52 +0100

    add git aliases to powershell

--- Profile.ps1
+++ Profile.ps1
@@ -12,3 +12,15 @@ function Prompt {
 	Write-Host "] ->" -ForegroundColor Blue -NoNewline
 	return " "
 }
+
+function Get-GitStatus { & git status }
+New-Alias -Name wow -Value Get-GitStatus
+function Get-GitDiff { & git diff }
+New-Alias -Name gd -Value Get-GitDiff
+function Get-GitLog { & git log }
+Remove-Item alias:gl -Force
+New-Alias -Name gl -Value Get-GitLog
+function Get-GitBranches { & git branch }
+New-Alias -Name gb -Value Get-GitBranches
+function Do-GitPullOriginMaster { & git pull origin master }
+New-Alias -Name gpom -Value Do-GitPullOriginMaster
