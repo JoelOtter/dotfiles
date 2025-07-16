558e105c4785a5ef36cf643682ae4dbeb366f80d
Joel Auterson <joel.auterson@gmail.com>
2025-07-16 12:19:19 +0100

    Fix airline colour

--- neovim/lua/plugins/base16-vim.lua
+++ neovim/lua/plugins/base16-vim.lua
@@ -3,6 +3,11 @@ local Plugin = { 'tinted-theming/tinted-vim' }
 function Plugin.init()
   vim.g.tinted_colorspace = 256
   vim.cmd.colorscheme('base16-materia')
+
+  -- Airline uses the difftext colour to set its own colours for some reason
+  vim.cmd [[
+    hi DiffText ctermfg=4 ctermbg=18 guifg=#89ddff guibg=#2c393f
+  ]]
 end
 
 Plugin.priority = 1000
